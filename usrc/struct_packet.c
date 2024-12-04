1ae0e3f03b281044bd0d2ae3406e029aca8bc3ad
Jacem Haggui <jacem.haggui@eurecom.fr>
2024-12-04 22:46:10 +0100

    Moved the CmdlinetoPacket function to the struct_packet.c file

--- usrc/struct_packet.c
+++ usrc/struct_packet.c
@@ -155,7 +155,7 @@ int packet_to_string(Packet * packet, char * string) {
 	return 0;
 }
 
-
+/*
 void main() {
 
 
@@ -174,7 +174,7 @@ void main() {
         	string5,
         	string6
 	};
-	/*
+	
 	for (int i = 1; i < 7 ; i ++) { 
 		Packet * packet = empty_packet();
 		int code = string_to_packet(string_list[i-1], packet);
@@ -182,7 +182,7 @@ void main() {
 		print_packet(packet);
 		printf("\nCode %d\n", code);
 	} 
-	*/
+	
 	//Packet * packet = empty_packet();
 	int code = string_to_packet(string_list[0], packet);
 	print_packet(packet);
@@ -193,3 +193,86 @@ void main() {
 	print_string(string_receiver, n);
 	free_packet(packet);
 }
+
+*/
+
+int CmdlinetoPacket(const char *input, Packet *pkt) {
+    // Initialize the packet with fixed values
+    pkt->E = 'E';
+    pkt->D = 'D';
+    pkt->r = 'r';
+    
+    // Initializing data_size and the option1 and option2 arrays to zero.
+    // We use memset here to efficiently set all 32 bytes of each array to 0.
+    // This ensures that the options are empty before we populate them with actual data.
+    memset(pkt->option1, 0, sizeof(pkt->option1));
+    memset(pkt->option2, 0, sizeof(pkt->option2));
+    pkt->data_size = 0;
+
+    // Split input into command and arguments
+    // Buffers to store command and arguments
+    char command[64];  // Size 64 for most commands
+    char option1[256]; // Size 256 for first argument (e.g., filename)
+    char option2[256]; // Size 256 for second argument (if needed)
+
+    // Parse the input into command and options
+    int args = sscanf(input, "%s %s %s", command, option1, option2); 
+    /* sscanf Explanation
+    * The sscanf function is used to read formatted input from the string `input`.
+    * 
+    * - "%s %s %s" is the format string that tells sscanf to:
+    *   - Read a string (word) up to the first space and store it in `command`
+    *   - Read the next string up to the next space and store it in `option1`
+    *   - Read the next string up to the next space and store it in `option2`
+    * 
+    * In the case of an input like "put myfile.txt /path/to/destination":
+    * - `command` will store "put"
+    * - `option1` will store "myfile.txt"
+    * - `option2` will store "/path/to/destination"
+    * 
+    * `args` will store the number of successful assignments, which is 3 in this case 
+    * because three strings were successfully extracted from the `input`.
+    */
+
+    strncpy(pkt->option1, option1, sizeof(pkt->option1) - 1);  // Copy the first option (e.g., filename)
+    strncpy(pkt->option2, option2, sizeof(pkt->option2) - 1);  // Copy the second option (if available)
+
+    // Determine which command was entered
+    if (strcmp(command, "put") == 0) {
+        pkt->code = 1;  // Command code for "put" (not permanent)
+        //TODO: Put the entire file contents in data
+        //Seems weird since the packet only takes a pointer to data ?
+        //Reminder: Discuss with the group 
+    } 
+    else if (strcmp(command, "rm") == 0) {
+        pkt->code = 2;  // Command code for "rm"
+    } 
+    else if (strcmp(command, "get") == 0) {
+        pkt->code = 3;  // Command code for "get"
+        // pkt->option1 and pkt->option2 have already been set earlier
+    } 
+    else if (strcmp(command, "ls") == 0) {
+        pkt->code = 4;  // Command code for "ls"
+    } 
+    else if (strcmp(command, "cat") == 0) {
+        pkt->code = 5;  // Command code for "cat"
+    } 
+    else if (strcmp(command, "mv") == 0) {
+        pkt->code = 6;  // Command code for "mv"
+        // pkt->option1 and pkt->option2 have already been set earlier
+    } 
+    else if (strcmp(command, "quit") == 0 || strcmp(command, "exit") == 0) {
+        pkt->code = 7;  // Command byte for "quit" or "exit"
+    } 
+    else if (strcmp(command, "restart") == 0) {
+        pkt->code = 8;  // Command byte for "restart"
+    }
+
+    else {
+        fprintf(stderr, "Error: Unknown command '%s'\n", command);
+        return -1;
+    }
+
+    return 1;  // Indicate success
+}
+
