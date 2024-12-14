47386fd924fad93dd5e7bc6792919155cbb81a40
mougeot <clara.mougeot@eurecom.fr>
2024-12-14 14:11:09 +0100

    Modified the tabulations AGAIN

--- usrc/functions.c
+++ usrc/functions.c
@@ -14,12 +14,12 @@
 
 char *  cats(char* dest, char* source){
   /*  
-  Concatenate dest and source into a new string both.
+    Concatenate dest and source into a new string both.
   INPUT :
-      dest : Source string that will be copied into a new string
-      source : Source string that will be concatenated to the end of dest
+    dest : Source string that will be copied into a new string
+    source : Source string that will be concatenated to the end of dest
   OUTPUT :
-      both : New string containing the concatenation of dest and source
+    both : New string containing the concatenation of dest and source
 	*/
   char* both = malloc(sizeof(char) * (strlen(source) + strlen(dest)));
   strcpy(both, dest);
@@ -30,12 +30,12 @@ char *  cats(char* dest, char* source){
 
 char* itoa(int val, int base) {
   /*  
-      Converts an int to string.
-	INPUT :
-	    val : The integer value to be converted
-      base : The numerical base for the conversion
-	OUTPUT :
-      &buf[i+1] : Pointer to the string representation of the integer in the specified base
+    Converts an int to string.
+  INPUT :
+    val : The integer value to be converted
+    base : The numerical base for the conversion
+  OUTPUT :
+    &buf[i+1] : Pointer to the string representation of the integer in the specified base
 	*/
 	static char buf[32] = {0};
 	int i = 30;
@@ -47,11 +47,11 @@ char* itoa(int val, int base) {
 
 int line_count(char string[]) {
   /*  
-      Returns the number of lines in a string.
-	INPUT :
-	    string[] : The string to analyze
-	OUTPUT :
-      c : Number of lines in the string
+    Returns the number of lines in a string.
+  INPUT :
+    string[] : The string to analyze
+  OUTPUT :
+    c : Number of lines in the string
 	*/
   int leng = strlen(string);
   if (leng == 0) {
@@ -78,10 +78,10 @@ char * CLIENT_DIRECTORY = NULL; // DEFINE NULL at the beginning (Will be updated
 
 void set_server_directory(const char *string) {
 	/*  
-      Set the directory path for the server side.
-	INPUT :
-	    string : The New Absolute Path (or relative in fact) to the server working directory
-	OUTPUT :
+    Set the directory path for the server side.
+  INPUT :
+    string : The New Absolute Path (or relative in fact) to the server working directory
+  OUTPUT :
 	*/
   // IF SERVER_DIRECTORY IS ALREADY FILLED -> FREE IT
   if (SERVER_DIRECTORY != NULL) {
@@ -102,9 +102,9 @@ void set_server_directory(const char *string) {
 
 void set_client_directory(const char *string) {
 	/*  
-      Set the directory path for the client side.
+    Set the directory path for the client side.
   INPUT :
-	    string : The New Absolute Path (or relative in fact) to the client working directory
+    string : The New Absolute Path (or relative in fact) to the client working directory
   OUTPUT :
 	*/
     // IF CLIENT_DIRECTORY IS ALREADY FILLED -> FREE IT
@@ -126,9 +126,9 @@ void set_client_directory(const char *string) {
 
 void force_server_directory_format(){
 	/*  
-      Apply the convention to the server directory string, Must ends with '/', and change it if it's not the case.
-	INPUT :
-	OUTPUT :
+    Apply the convention to the server directory string, Must ends with '/', and change it if it's not the case.
+  INPUT :
+  OUTPUT :
 	*/
 	if (SERVER_DIRECTORY == NULL) {
 		printf("SERVER DIRECTORY PATH == NULL");
@@ -144,9 +144,9 @@ void force_server_directory_format(){
 
 void force_client_directory_format(){
 	/*  
-      Apply the convention to the client directory string, Must ends with '/', and change it if it's not the case.
-	INPUT :
-	OUTPUT :
+    Apply the convention to the client directory string, Must ends with '/', and change it if it's not the case.
+  INPUT :
+  OUTPUT :
 	*/
 	if (CLIENT_DIRECTORY == NULL) {
 		printf("CLIENT DIRECTORY PATH == NULL");
@@ -162,14 +162,14 @@ void force_client_directory_format(){
 
 int convert_cmd_string_to_packet_string(char * cmd, char * string) {
   /*
-      Convert a string to a packet format.
-      The function uses a defined packet structure to transform a command-line string into a structured packet, then converts the packet into a string format.
+    Convert a string to a packet format.
+    The function uses a defined packet structure to transform a command-line string into a structured packet, then converts the packet into a string format.
   INPUT :
-      cmd : The command-line string to be converted
-      string : A buffer to store the resulting packet string
+    cmd : The command-line string to be converted
+    string : A buffer to store the resulting packet string
   OUTPUT :
-      error_code : Error code from the packet_to_string function
-      0 : Success
+    error_code : Error code from the packet_to_string function
+    0 : Success
   */
   Packet * packet = empty_packet();
 
